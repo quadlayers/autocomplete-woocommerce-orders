8a9010ecd6c7ab08e28b6f8eac6fccfa4a774069
Juan <6652831+quadlayers@users.noreply.github.com>
2022-04-24 20:19:05 -0300

    1.5.1

--- autocomplete-woocommerce-orders.php
+++ autocomplete-woocommerce-orders.php
@@ -4,48 +4,48 @@
  * Plugin Name: Autocomplete WooCommerce Orders
  * Plugin URI:  https://wordpress.org/plugins/autocomplete-woocommerce-orders/
  * Description: Autocomplete your WooCommerce orders with ease. This plugins works with most major payment providers including PayPal, SagePay and Stripe.
- * Version:     1.5.0
+ * Version:     1.5.1
  * Author:      QuadLayers
  * Author URI:  https://quadlayers.com
  * License:     GPLv3
  * Text Domain: autocomplete-woocommerce-orders
  * Domain Path:       /languages
  * WC requires at least: 3.1.0
- * WC tested up to: 6.2
+ * WC tested up to: 6.4
  */
 
 
-define('ACO_PLUGIN_NAME', 'Autocomplete WooCommerce Orders');
+define( 'ACO_PLUGIN_NAME', 'Autocomplete WooCommerce Orders' );
 
-define('ACO_PLUGIN_VERSION', '1.5.0');
+define( 'ACO_PLUGIN_VERSION', '1.5.1' );
 
-define('ACO_PLUGIN_FILE', __FILE__);
+define( 'ACO_PLUGIN_FILE', __FILE__ );
 
-define('ACO_PLUGIN_DIR', __DIR__ . DIRECTORY_SEPARATOR);
+define( 'ACO_PLUGIN_DIR', __DIR__ . DIRECTORY_SEPARATOR );
 
-define('ACO_PREFIX', 'aco');
+define( 'ACO_PREFIX', 'aco' );
 
-define('ACO_WORDPRESS_URL', 'https://wordpress.org/plugins/autocomplete-woocommerce-orders/');
+define( 'ACO_WORDPRESS_URL', 'https://wordpress.org/plugins/autocomplete-woocommerce-orders/' );
 
-define('ACO_REVIEW_URL', 'https://wordpress.org/support/plugin/autocomplete-woocommerce-orders/reviews/?filter=5#new-post');
+define( 'ACO_REVIEW_URL', 'https://wordpress.org/support/plugin/autocomplete-woocommerce-orders/reviews/?filter=5#new-post' );
 
-define('ACO_DOCUMENTATION_URL', 'https://quadlayers.com/documentation/woocommerce-autocomplete-orders/?utm_source=aco_admin');
+define( 'ACO_DOCUMENTATION_URL', 'https://quadlayers.com/documentation/woocommerce-autocomplete-orders/?utm_source=aco_admin' );
 
-define('ACO_DEMO_URL', 'https://quadlayers.com/portfolio/woocommerce-autocomplete-orders/?utm_source=aco_admin');
+define( 'ACO_DEMO_URL', 'https://quadlayers.com/portfolio/woocommerce-autocomplete-orders/?utm_source=aco_admin' );
 
-define('ACO_PURCHASE_URL', ACO_DEMO_URL);
+define( 'ACO_PURCHASE_URL', ACO_DEMO_URL );
 
-define('ACO_SUPPORT_URL', 'https://quadlayers.com/account/support/?utm_source=aco_admin');
+define( 'ACO_SUPPORT_URL', 'https://quadlayers.com/account/support/?utm_source=aco_admin' );
 
-define('ACO_GROUP_URL', 'https://www.facebook.com/groups/quadlayers');
+define( 'ACO_GROUP_URL', 'https://www.facebook.com/groups/quadlayers' );
 
 // Include the main ACO class.
-if (!class_exists('ACO')) {
-	include_once dirname(__FILE__) . '/includes/class-aco.php';
+if ( ! class_exists( 'ACO' ) ) {
+	include_once dirname( __FILE__ ) . '/includes/class-aco.php';
 }
 
-if (!class_exists('QL_Widget')) {
-	include_once(ACO_PLUGIN_DIR . 'includes/quadlayers/widget.php');
+if ( ! class_exists( 'QL_Widget' ) ) {
+	include_once ACO_PLUGIN_DIR . 'includes/quadlayers/widget.php';
 }
 
 
@@ -58,8 +58,7 @@ if (!class_exists('QL_Widget')) {
  * @return ACO
  */
 
-function aco()
-{
+function aco() {
 	return ACO::instance();
 }
 
