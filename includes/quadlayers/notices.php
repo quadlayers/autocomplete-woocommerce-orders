7813ab4dec772377a2d3ffb2e59849142378f508
Juan <6652831+quadlayers@users.noreply.github.com>
2022-09-02 10:59:05 -0300

    1.5.7

--- includes/quadlayers/notices.php
+++ includes/quadlayers/notices.php
@@ -5,18 +5,18 @@ class ACO_Notices {
 	protected static $_instance;
 
 	public function __construct() {
-		add_action( 'wp_ajax_aco_dismiss_notice', array( $this, 'ajax_dismiss_notice' ) );
+		add_action( 'wp_ajax_wooccm_dismiss_notice', array( $this, 'ajax_dismiss_notice' ) );
 		add_action( 'admin_notices', array( $this, 'add_notices' ) );
 		register_activation_hook( ACO_PLUGIN_FILE, array( $this, 'add_transient' ) );
 	}
 
 	function ajax_dismiss_notice() {
-		if ( check_admin_referer( 'aco_dismiss_notice', 'nonce' ) && isset( $_REQUEST['notice_id'] ) ) {
+		if ( check_admin_referer( 'wooccm_dismiss_notice', 'nonce' ) && isset( $_REQUEST['notice_id'] ) ) {
 
 			$notice_id = sanitize_key( $_REQUEST['notice_id'] );
 
 			update_user_meta( get_current_user_id(), $notice_id, true );
-			set_transient( 'aco-notice-delay', true, MONTH_IN_SECONDS );
+			set_transient( 'wooccm-notice-delay', true, MONTH_IN_SECONDS );
 
 			wp_send_json( $notice_id );
 		}
@@ -25,12 +25,12 @@ class ACO_Notices {
 	}
 
 	function add_transient() {
-		set_transient( 'aco-notice-delay', true, MONTH_IN_SECONDS );
+		set_transient( 'wooccm-notice-delay', true, MONTH_IN_SECONDS );
 	}
 
 	function add_notices() {
 
-		$transient = get_transient( 'aco-notice-delay' );
+		$transient = get_transient( 'wooccm-notice-delay' );
 
 		if ( $transient ) {
 			return;
@@ -40,7 +40,7 @@ class ACO_Notices {
 		<script>
 			(function($) {
 				$(document).ready(()=> {
-					$('.aco-notice').on('click', '.notice-dismiss', function(e) {
+					$('.wooccm-notice').on('click', '.notice-dismiss', function(e) {
 						e.preventDefault();
 						var notice_id = $(e.delegateTarget).data('notice_id');
 						$.ajax({
@@ -48,8 +48,8 @@ class ACO_Notices {
 						url: ajaxurl,
 						data: {
 							notice_id: notice_id,
-							action: 'aco_dismiss_notice',
-							nonce: '<?php echo esc_attr( wp_create_nonce( 'aco_dismiss_notice' ) ); ?>'
+							action: 'wooccm_dismiss_notice',
+							nonce: '<?php echo esc_attr( wp_create_nonce( 'wooccm_dismiss_notice' ) ); ?>'
 						},
 							success: function(response) {
 							console.log(response);
@@ -61,15 +61,15 @@ class ACO_Notices {
 		</script>
 		<?php
 
-		$plugin_slug = ACO_CROSS_SELL_SLUG;
+		$plugin_slug = ACO_PREMIUM_SELL_SLUG;
 
-		$user_rating     = ! get_user_meta( get_current_user_id(), 'aco-user-rating', true );
-		$user_premium    = ! get_user_meta( get_current_user_id(), 'aco-user-premium', true ) && ! $this->is_installed( "{$plugin_slug}/{$plugin_slug}.php" );
-		$user_cross_sell = ! get_user_meta( get_current_user_id(), 'aco-user-cross-sell', true );
+		$user_rating     = ! get_user_meta( get_current_user_id(), 'wooccm-user-rating', true );
+		$user_premium    = ! get_user_meta( get_current_user_id(), 'wooccm-user-premium', true ) && ! $this->is_installed( "{$plugin_slug}/{$plugin_slug}.php" );
+		$user_cross_sell = ! get_user_meta( get_current_user_id(), 'wooccm-user-cross-sell', true );
 
 		if ( $user_rating ) {
 			?>
-			<div id="aco-admin-rating" class="aco-notice notice notice-info is-dismissible" data-notice_id="aco-user-rating">
+			<div id="wooccm-admin-rating" class="wooccm-notice notice notice-info is-dismissible" data-notice_id="wooccm-user-rating">
 				<div class="notice-container" style="padding-top: 10px; padding-bottom: 10px; display: flex; justify-content: left; align-items: center;">
 					<div class="notice-image">
 						<img style="border-radius:50%;max-width: 90px;" src="<?php echo plugins_url( '/assets/backend/img/logo.jpg', ACO_PLUGIN_FILE ); ?>" alt="<?php echo esc_html( ACO_PLUGIN_NAME ); ?>>">
@@ -95,7 +95,7 @@ class ACO_Notices {
 
 		if ( ! $user_rating && $user_premium ) {
 			?>
-			<div class="aco-notice notice notice-info is-dismissible" data-notice_id="aco-user-premium">
+			<div class="wooccm-notice notice notice-info is-dismissible" data-notice_id="wooccm-user-premium">
 				<div class="notice-container" style="padding-top: 10px; padding-bottom: 10px; display: flex; justify-content: left; align-items: center;">
 					<div class="notice-image">
 						<img style="border-radius:50%;max-width: 90px;" src="<?php echo esc_url( plugins_url( '/assets/backend/img/logo.jpg', ACO_PLUGIN_FILE ) ); ?>" alt="<?php echo esc_html( ACO_PLUGIN_NAME ); ?>>">
@@ -107,14 +107,14 @@ class ACO_Notices {
 						<?php
 						printf(
 							esc_html__( 'Today we want to make you a special gift. Using this coupon before the next 48 hours you can get a 20 percent discount on the premium version of the %s plugin.', 'autocomplete-woocommerce-orders' ),
-							esc_html( ACO_CROSS_SELL_NAME )
+							esc_html( ACO_PREMIUM_SELL_NAME )
 						)
 						?>
 						</p>
-						<a href="<?php echo esc_url( ACO_CROSS_SELL_URL ); ?>" class="button-primary" target="_blank">
+						<a href="<?php echo esc_url( ACO_PREMIUM_SELL_URL ); ?>" class="button-primary" target="_blank">
 							<?php esc_html_e( 'More info', 'autocomplete-woocommerce-orders' ); ?>
 						</a>
-						<input style="width:95px" type="text" value="ADMINPANEL20%"/>
+						<input style="width:130px" type="text" value="ADMINPANEL20%"/>
 					</div>
 				</div>
 			</div>
@@ -133,21 +133,21 @@ class ACO_Notices {
 			list($action, $action_link) = $cross_sell;
 
 			?>
-			<div class="aco-notice notice notice-info is-dismissible" data-notice_id="aco-user-cross-sell">
+			<div class="wooccm-notice notice notice-info is-dismissible" data-notice_id="wooccm-user-cross-sell">
 				<div class="notice-container" style="padding-top: 10px; padding-bottom: 10px; display: flex; justify-content: left; align-items: center;">
 					<div class="notice-image">
 						<img style="border-radius:50%;max-width: 90px;" src="<?php echo plugins_url( '/assets/backend/img/logo.jpg', ACO_PLUGIN_FILE ); ?>" alt="<?php echo esc_html( ACO_PLUGIN_NAME ); ?>>">
 					</div>
 					<div class="notice-content" style="margin-left: 15px;">
 						<p>
-						<?php printf( esc_html__( 'Hello! We want to invite you to try our %s plugin!', 'autocomplete-woocommerce-orders' ), ACO_CROSS_SELL_NAME ); ?>
+						<?php printf( esc_html__( 'Hello! We want to invite you to try our %s plugin!', 'autocomplete-woocommerce-orders' ), esc_html( ACO_CROSS_INSTALL_NAME ) ); ?>
 							<br/>
-						<?php echo esc_html( ACO_CROSS_SELL_DESCRIPTION ); ?>
+						<?php echo esc_html( ACO_CROSS_INSTALL_DESCRIPTION ); ?>
 						</p>
 						<a href="<?php echo esc_url( $action_link ); ?>" class="button-primary">
 						<?php echo esc_html( $action ); ?>
 						</a>
-						<a href="<?php echo esc_url( ACO_CROSS_SELL_URL ); ?>" class="button-secondary" target="_blank">
+						<a href="<?php echo esc_url( ACO_CROSS_INSTALL_URL ); ?>" class="button-secondary" target="_blank">
 						<?php esc_html_e( 'More info', 'autocomplete-woocommerce-orders' ); ?>
 						</a>
 					</div>
