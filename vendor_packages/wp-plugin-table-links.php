449430d1f544378ce6f19a46b3b206ed18c96496
Juan <6652831+quadlayers@users.noreply.github.com>
2023-02-13 15:45:53 -0300

    Composer packages update
    Composer packages update

--- vendor_packages/wp-plugin-table-links.php
+++ vendor_packages/wp-plugin-table-links.php
@@ -1,27 +1,27 @@
 <?php
 
-if (class_exists('QuadLayers\\WP_Plugin_Table_Links\\Load')) {
+if ( class_exists( 'QuadLayers\\WP_Plugin_Table_Links\\Load' ) ) {
 	new \QuadLayers\WP_Plugin_Table_Links\Load(
 		ACO_PLUGIN_FILE,
 		array(
 			array(
-				'text' => esc_html__('Settings', 'autocomplete-woocommerce-orders'),
-				'url'  => admin_url('admin.php?page=wc-settings&tab=silkwave_aco'),
+				'text' => esc_html__( 'Settings', 'autocomplete-woocommerce-orders' ),
+				'url'  => admin_url( 'admin.php?page=wc-settings&tab=silkwave_aco' ),
 			),
 			array(
-				'text' => esc_html__('Premium', 'autocomplete-woocommerce-orders'),
+				'text' => esc_html__( 'Premium', 'autocomplete-woocommerce-orders' ),
 				'url'  => ACO_PREMIUM_SELL_URL,
 			),
 			array(
 				'place' => 'row_meta',
-				'text'  => esc_html__('Support', 'autocomplete-woocommerce-orders'),
+				'text'  => esc_html__( 'Support', 'autocomplete-woocommerce-orders' ),
 				'url'   => ACO_SUPPORT_URL,
 			),
 			array(
 				'place' => 'row_meta',
-				'text'  => esc_html__('Documentation', 'autocomplete-woocommerce-orders'),
+				'text'  => esc_html__( 'Documentation', 'autocomplete-woocommerce-orders' ),
 				'url'   => ACO_DOCUMENTATION_URL,
 			),
-		),
+		)
 	);
 }
