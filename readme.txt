c9550e9e7251d9b42dee61fbdc4debeb8704283f
Francisco <6652831+franmastromarino@users.noreply.github.com>
2024-11-26 12:45:26 -0300

    fix: version update

--- readme.txt
+++ readme.txt
@@ -4,12 +4,12 @@ Donate link: https://quadlayers.com/products/woocommerce-autocomplete-orders/
 Tags: WooCommerce, Order Autocomplete, Virtual Products, Payment Integration
 Requires at least: 4.7
 Requires PHP: 5.6
-Tested up to: 6.6
-Stable tag: 3.3.6
+Tested up to: 6.7
+Stable tag: 3.3.7
 License: GPLv3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 WC requires at least: 4.0
-WC tested up to: 9.3
+WC tested up to: 9.4
 
 Enhance your WooCommerce store with Autocomplete Orders. Automatically complete orders after payment, perfect for virtual goods and subscriptions.
 
@@ -63,6 +63,9 @@ The plugin supports all major payment gateways that are compatible with WooComme
 7. Pick the mode that suits you best
 
 == Changelog ==
+
+= 3.3.7 =
+* WooCommerce compatibility
 
 = 3.3.6 =
 * WooCommerce compatibility
--- vendor_packages/wp-notice-plugin-promote.php
+++ vendor_packages/wp-notice-plugin-promote.php
@@ -22,6 +22,7 @@ if ( class_exists( 'QuadLayers\\WP_Notice_Plugin_Promote\\Load' ) ) {
 	define( 'ACO_PROMOTE_CROSS_INSTALL_1_NAME', 'WooCommerce Checkout Manager' );
 	define( 'ACO_PROMOTE_CROSS_INSTALL_1_DESCRIPTION', esc_html__( 'This plugin allows you to add custom fields to the checkout page, related to billing, shipping or additional fields sections.', 'autocomplete-woocommerce-orders' ) );
 	define( 'ACO_PROMOTE_CROSS_INSTALL_1_URL', 'https://quadlayers.com/products/woocommerce-checkout-manager/?utm_source=aco_admin' );
+	define( 'ACO_PROMOTE_CROSS_INSTALL_1_LOGO_SRC', plugins_url( '/assets/backend/img/woocommerce-checkout-manager.jpg', ACO_PLUGIN_FILE ) );
 	/**
 	 * Notice cross sell 2
 	 */
@@ -29,90 +30,79 @@ if ( class_exists( 'QuadLayers\\WP_Notice_Plugin_Promote\\Load' ) ) {
 	define( 'ACO_PROMOTE_CROSS_INSTALL_2_NAME', 'WooCommerce Direct Checkout' );
 	define( 'ACO_PROMOTE_CROSS_INSTALL_2_DESCRIPTION', esc_html__( 'It allows you to reduce the steps in the checkout process by skipping the shopping cart page. This can encourage buyers to shop more and quickly. You will increase your sales reducing cart abandonment.', 'autocomplete-woocommerce-orders' ) );
 	define( 'ACO_PROMOTE_CROSS_INSTALL_2_URL', 'https://quadlayers.com/products/woocommerce-direct-checkout/?utm_source=aco_admin' );
+	define( 'ACO_PROMOTE_CROSS_INSTALL_2_LOGO_SRC', plugins_url( '/assets/backend/img/woocommerce-direct-checkout.jpg', ACO_PLUGIN_FILE ) );
 
 	new \QuadLayers\WP_Notice_Plugin_Promote\Load(
 		ACO_PLUGIN_FILE,
 		array(
 			array(
 				'type'               => 'ranking',
-				'notice_delay'       => MONTH_IN_SECONDS,
+				'notice_delay'       => 0,
 				'notice_logo'        => ACO_PROMOTE_LOGO_SRC,
-				'notice_title'       => sprintf(
-					esc_html__(
-						'Hello! Thank you for choosing the %s plugin!',
-						'autocomplete-woocommerce-orders'
-					),
-					ACO_PLUGIN_NAME
+				'notice_description' => sprintf(
+								esc_html__( 'Hello! %2$s We\'ve spent countless hours developing this free plugin for you and would really appreciate it if you could drop us a quick rating. Your feedback is extremely valuable to us. %3$s It helps us to get better. Thanks for using %1$s.', 'perfect-woocommerce-brands' ),
+								'<b>'.ACO_PLUGIN_NAME.'</b>',
+								'<span style="font-size: 16px;">🙂</span>',
+								'<br>'
 				),
-				'notice_description' => esc_html__( 'Could you please give it a 5-star rating on WordPress? Your feedback boosts our motivation, helps us promote, and continues to improve this product. Your support matters!', 'autocomplete-woocommerce-orders' ),
 				'notice_link'        => ACO_PROMOTE_REVIEW_URL,
-				'notice_link_label'  => esc_html__(
-					'Yes, of course!',
-					'autocomplete-woocommerce-orders'
-				),
 				'notice_more_link'   => ACO_SUPPORT_URL,
 				'notice_more_label'  => esc_html__(
 					'Report a bug',
-					'autocomplete-woocommerce-orders'
+					'perfect-woocommerce-brands'
 				),
 			),
 			array(
 				'plugin_slug'        => ACO_PROMOTE_PREMIUM_SELL_SLUG,
-				'notice_delay'       => MONTH_IN_SECONDS,
+				'plugin_install_link'   => ACO_PROMOTE_PREMIUM_SELL_URL,
+				'plugin_install_label'  => esc_html__(
+					'Purchase Now',
+					'perfect-woocommerce-brands'
+				),
+				'notice_delay'       => WEEK_IN_SECONDS,
 				'notice_logo'        => ACO_PROMOTE_LOGO_SRC,
 				'notice_title'       => esc_html__(
 					'Hello! We have a special gift!',
-					'autocomplete-woocommerce-orders'
+					'perfect-woocommerce-brands'
 				),
 				'notice_description' => sprintf(
 					esc_html__(
-						'Today we want to make you a special gift. Using the coupon code %1$s before the next 48 hours you can get a 20 percent discount on the premium version of the %2$s plugin.',
-						'autocomplete-woocommerce-orders'
+						'Today we have a special gift for you. Use the coupon code %1$s within the next 48 hours to receive a %2$s discount on the premium version of the %3$s plugin.',
+						'perfect-woocommerce-brands'
 					),
 					'ADMINPANEL20%',
+					'20%',
 					ACO_PROMOTE_PREMIUM_SELL_NAME
 				),
 				'notice_more_link'   => ACO_PROMOTE_PREMIUM_SELL_URL,
-				'notice_more_label'  => esc_html__(
-					'More info!',
-					'autocomplete-woocommerce-orders'
-				),
 			),
 			array(
 				'plugin_slug'        => ACO_PROMOTE_CROSS_INSTALL_1_SLUG,
-				'notice_delay'       => MONTH_IN_SECONDS * 4,
-				'notice_logo'        => ACO_PROMOTE_LOGO_SRC,
+				'notice_delay'       => MONTH_IN_SECONDS * 3,
+				'notice_logo'        => ACO_PROMOTE_CROSS_INSTALL_1_LOGO_SRC,
 				'notice_title'       => sprintf(
 					esc_html__(
 						'Hello! We want to invite you to try our %s plugin!',
-						'autocomplete-woocommerce-orders'
+						'perfect-woocommerce-brands'
 					),
 					ACO_PROMOTE_CROSS_INSTALL_1_NAME
 				),
 				'notice_description' => ACO_PROMOTE_CROSS_INSTALL_1_DESCRIPTION,
-				'notice_more_link'   => ACO_PROMOTE_CROSS_INSTALL_1_URL,
-				'notice_more_label'  => esc_html__(
-					'More info!',
-					'autocomplete-woocommerce-orders'
-				),
+				'notice_more_link'   => ACO_PROMOTE_CROSS_INSTALL_1_URL
 			),
 			array(
 				'plugin_slug'        => ACO_PROMOTE_CROSS_INSTALL_2_SLUG,
 				'notice_delay'       => MONTH_IN_SECONDS * 6,
-				'notice_logo'        => ACO_PROMOTE_LOGO_SRC,
+				'notice_logo'        => ACO_PROMOTE_CROSS_INSTALL_2_LOGO_SRC,
 				'notice_title'       => sprintf(
 					esc_html__(
 						'Hello! We want to invite you to try our %s plugin!',
-						'autocomplete-woocommerce-orders'
+						'perfect-woocommerce-brands'
 					),
 					ACO_PROMOTE_CROSS_INSTALL_2_NAME
 				),
 				'notice_description' => ACO_PROMOTE_CROSS_INSTALL_2_DESCRIPTION,
-				'notice_more_link'   => ACO_PROMOTE_CROSS_INSTALL_2_URL,
-				'notice_more_label'  => esc_html__(
-					'More info!',
-					'autocomplete-woocommerce-orders'
-				),
+				'notice_more_link'   => ACO_PROMOTE_CROSS_INSTALL_2_URL
 			),
 		)
 	);
